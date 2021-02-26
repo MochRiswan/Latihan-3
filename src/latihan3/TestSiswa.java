b20b643c193984d316cb052012783729b06ff607
MochRiswan <moch_riswan_29rpl@studen.smktelkom-mlg.sch.id>
2021-02-26 16:49:02 +0700

    Latihan 3

---
+++ src/latihan3/TestSiswa.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan3;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class TestSiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        EncapSiswa siswa = new EncapSiswa();
+        siswa.setName("Julian");
+        siswa.setAbsen(23);
+        siswa.setAddress("Malang");
+        
+        System.out.println("Name : "+siswa.getName()+" Absen  : "+siswa.getAbsen()+" Address : "+siswa.getAddress());
+        
+    }
+    
+}
